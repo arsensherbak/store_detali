b553ad91b7d0b44cd0fcc72cb4403b7c9abe7ecb
arsensherbak <sherbakars@gmail.com>
2022-04-25 00:15:25 +0300

    check connection

---
+++ app/Http/Controllers/Main/Show/ShoController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Main\Show;
+
+use App\Http\Controllers\Controller;
+
+use App\Models\Product;
+
+
+
+class ShoController extends Controller
+{
+    public function __invoke(Product $product)
+    {
+
+        return view('main.prod', compact('product'));
+    }
+}
