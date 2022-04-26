4b6598eb4488165f9a299aaa0138e50b00166a28
arsensherbak <sherbakars@gmail.com>
2022-04-27 01:22:32 +0300

    add class Resource

--- app/Http/Controllers/Admin/Admin/StoreController.php
+++ app/Http/Controllers/Admin/Admin/StoreController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Admin\StoreRequest;
 
+use App\Http\Resources\Admin\AdminResource;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
@@ -15,7 +16,9 @@ class StoreController extends Controller
     {
         $data = $request->validated();
         $data['password'] = Hash::make($data['password']);
-        User::firstOrCreate(['email' => $data['email']], $data);
-        return redirect()->route('admin.user.index');
+        $user = User::firstOrCreate(['email' => $data['email']], $data);
+
+        return new AdminResource($user);
+//        return redirect()->route('admin.user.index');
     }
 }
